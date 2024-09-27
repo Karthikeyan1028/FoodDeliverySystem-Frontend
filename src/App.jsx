5e21111435d927c348d68ed97f44583ae5a7b6cd
Karthikeyan1028 <karthikidd03@gmail.com>
2024-09-28 00:45:34 +0530

    path changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './components/Navbar/NavBar.jsx'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/NavBar/NavBar.jsx'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
