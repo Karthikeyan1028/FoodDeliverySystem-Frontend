a85e27fab74fca6c4c7d36beb1ea9ec84ab1392d
Karthikeyan1028 <karthikidd03@gmail.com>
2024-09-30 09:23:00 +0530

    resolving errors

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -9,7 +9,7 @@ function FoodItem ({id,name,price,description,image}) {
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
-            <img className='food-item-image' src={"public/images/"+image} alt="" />
+            <img className='food-item-image' src={"/images/"+image} alt="" />
             {!cartItems[id]
                 ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
                 :<div className='food-item-counter'>
